01b04168399ca7b553028f8b73d19964685e103e
Raúl Valverde Sánchez <raulvalverdesanchez@gmail.com>
2017-10-04 12:01:05 +0200

    first commit puta madre

---
+++ MasterMind/src/Jugador.java
@@ -0,0 +1,16 @@
+
+public class Jugador {
+	String idJugador;
+	String nomJugador;
+	int partidesJugades;
+	int partidesGuanyades;
+	
+	public class Jugador(String idJugador, String nomJugador, int partidesJugades, int partidesGuanyades) {
+		this.idJugador = idJugador;
+		this.nomJugador = nomJugador;
+		this.partidesJugades = partidesJugades;
+		this.partidesGuanyades = partidesGuanyades;
+	}
+	
+	
+}
