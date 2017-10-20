cbb86bee92b9a121dceac56936e92ceaf13e2179
matiszz <matias.szbar@gmail.com>
2017-10-20 16:11:29 +0200

    Metodes Rellotge

--- MasterMind/src/Rellotge.java
+++ MasterMind/src/Rellotge.java
@@ -1,25 +1,48 @@
 public class Rellotge{
     int begin;
     int end;
+    int ellapsed;
 
+    /*
+        Inicia el rellotge.
+        < Retorna 1 per indicar que ha sigut correcte.
+     */
     public void iniciar_rellotge() {
         begin = System.currentTimeMillis();
+        return 1;
     }
 
+    /*
+        Continua el rellotge.
+        > Se li passa com a paràmetre el temps en millisegons.
+        < Retorna 1 per indicar que ha sigut correcte.
+     */
     public void continuar_rellotge(int time) {
         begin = time;
+        return 1;
     }
 
+    /*
+        Atura el rellotge.
+        < Retorna el temps en millisegons.
+     */
     public String aturar_rellotge() {
         end = System.currentTimeMillis();
-        int ellapsed = end - begin;
-        int milliseconds, seconds, minutes, hours;
-
-        hours = ellapsed/3600000.0;
+        ellapsed = end - begin;
+        return ellapsed;
     }
 
-
+    /*
+        Retorna el temps guardat.
+        < El retorna un string amb el temps en format hh:mm:ss
+     */
     public String getTime() {
-        return ""+hores+':'+minuts+':'+segons+':'+milisegons;
+        String hhmmss = String.format("%02d:%02d:%02d",
+                TimeUnit.MILLISECONDS.toHours(ellapsed),
+                TimeUnit.MILLISECONDS.toMinutes(ellapsed) -
+                        TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(ellapsed)),
+                TimeUnit.MILLISECONDS.toSeconds(ellapsed) -
+                        TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(ellapsed)));
+        return hhmmss;
     }
 }
