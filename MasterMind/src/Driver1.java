1591fc55ad08f4c29b32080bb51d5ef137247071
Raúl Valverde Sánchez <raulvalverdesanchez@gmail.com>
2017-10-22 18:46:40 +0200

    Driver1 modificado para creación de la db [Raúl]

--- MasterMind/src/Driver1.java
+++ MasterMind/src/Driver1.java
@@ -1,3 +1,6 @@
+package com.prop.domini;
+
+import com.prop.persistencia.Database;
 
 public class Driver1 {
 	//Driver para las clases GeneradorJoc,Joc,Partida
@@ -10,9 +13,19 @@ public class Driver1 {
 	 5.JugarPartida nova
 	 6.JugarPartida guardada
 	 */
-	public void main() {
-while(true) {
-			
-		}
+	static Database db;
+	
+	public static void main (String args[]) {
+		System.out.println("Benvingut a Mastermind! Selecciona una de les següents opcions:");
+		System.out.println("1. Crear generador de jocs");
+		System.out.println("2. Crear joc per defecte");
+		System.out.println("3. Crear joc personalitzat");
+		System.out.println("4. Crear partida");
+		System.out.println("5. Jugar partida nova");
+		System.out.println("6. Continuar partida guardada");
+		
+		db = new Database("MasterMind-Database.txt");
+		db.createDatabase();
+		
 	}
 }
