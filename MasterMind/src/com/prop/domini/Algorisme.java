712204627a14230ff81d03ac64650b8010241294
sergilao <sergi.lao97@gmail.com>
2017-11-01 13:36:50 +0100

    Algoritmo printa código con 1 columna menos :(

--- MasterMind/src/com/prop/domini/Algorisme.java
+++ MasterMind/src/com/prop/domini/Algorisme.java
@@ -3,65 +3,69 @@ package com.prop.domini;
 import java.util.ArrayList;
 
 public class Algorisme {
-	ArrayList<String> posibilitats;
-	ArrayList<Character> jugat;
+	ArrayList<ArrayList<Integer>> combinacions;
+	ArrayList<Integer> jugat;
 	
 		public Algorisme() {
-			posibilitats = new ArrayList<String>();
-			jugat = new ArrayList<Character>();
+			combinacions = new ArrayList<ArrayList<Integer>>();
+			jugat = new ArrayList<Integer>();
 		};
-	
-		public void printa(ArrayList<String> r) {
-			for(String s : r) {
-				System.out.println(s);
+		public void printa(ArrayList<Integer> a) {
+			for(Integer k : a) {
+				System.out.print(k);
+			}
+
+		}
+		
+		public void copiar( ArrayList<Integer> ori,  ArrayList<Integer> dest) {
+			for(int i = 0; i < ori.size(); ++i) {
+				int valor = ori.get(i);
+				dest.add(valor);
 			}
 		}
 		
-		public void genera(int pos,int columnes, ArrayList<Character> colors, ArrayList<Character> resultat){
-			if(pos == columnes) {
-				String e = new String();
-				for(int i = 0; i < columnes;++i) {
-					e += resultat.get(i);
+		public void genera_combinacions(int pos, int columnes,int colors, ArrayList<Integer> intermig) {
+			if(pos == columnes)
+				combinacions.add(intermig);
+			else {
+				ArrayList<Integer> copia = new ArrayList<Integer>();
+				copiar(intermig,copia);
+				for(int i = 0; i < colors; ++i) {
+					copia.add(i);
+					genera_combinacions(pos+1,columnes, colors,copia);
+					copia.remove(pos);
 				}
-				posibilitats.add(e);
+			}
+		}
+		
+		public ArrayList<Integer> calcula_jugada(int columnes,int colors, ArrayList<Integer> resposta){
+			ArrayList<Integer> intermig = new ArrayList<Integer>();
+			if(resposta.isEmpty()) {
+				genera_combinacions(0,columnes,colors,intermig);
+				return combinacions.get(0);
 			}
 			else {
-				for(Character col:colors) {
-					resultat.add(col);
-					genera(pos+1,columnes,colors,resultat);
-					resultat.remove(pos);
+				for(int p = 0; p < columnes; ++p) {
+					int color = resposta.get(p);
+					if(color == 0 || color == 1) { //Tot malament o posicio malament
+						int incorrecte = jugat.get(p);
+						for(int p1 = 0; p1 < combinacions.size(); ++p1) { 
+							ArrayList<Integer> comb  = combinacions.get(p1);
+							//Recorro i descarto si no la posibilitat te aquesta posicio igual
+							if(comb.get(p) == incorrecte) combinacions.remove(p1);
+						}
+					}
+					else { //Tot bé
+						int correcte = jugat.get(p);
+						for(int p1 = 0; p1 < combinacions.size();++p1) { 
+							ArrayList<Integer> comb  = combinacions.get(p1);
+							//Recorro i descarto si no la posibilitat no te aquesta posicio igual
+							if(comb.get(p) != correcte) combinacions.remove(p1);
+						}
+					}
 				}
+			return combinacions.get(3);
 			}
 		}
-		
-		public ArrayList<Character> genera_nova_jugada(ArrayList<Character> resposta){
-			//Analiza la respuesta para dar la siguiente jugada
-			ArrayList<Character> nova_jugada = new ArrayList<Character>();
-			for(Character c : resposta) {
-				if(c == B)
-					
-				else if( c == N)
-					
-				else {
-					
-				}
-			}
-			return resposta;
-		}
-		
-		public ArrayList<Character> basic(int primer,int columnes, ArrayList<Character>colors, ArrayList<Character> resposta){ 
-			//Devuelve ArrayList<Character>
-			
-			ArrayList<Character> intermedis = new ArrayList<Character>();
-			
-			if(primer == 1) {//Solo genera todas la soluciones una vez
-				genera(0,columnes,colors,intermedis); 
-				String jugada = posibilitats.get(0);
-				//Convertir String en arraylist<character>
-				return jugat;
-			}
-			else { //Hago otra jugada a partir de la respuesta
-				return genera_nova_jugada(resposta);
-			}
-		}
+	
 }
