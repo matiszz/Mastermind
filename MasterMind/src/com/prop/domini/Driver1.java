34a43ceb0b6339f23c5ee42b793bc432882e8bea
matiszz <matias.szbar@gmail.com>
2017-11-09 12:23:00 +0100

    Driver1 arreglat

--- MasterMind/src/com/prop/domini/Driver1.java
+++ MasterMind/src/com/prop/domini/Driver1.java
@@ -8,9 +8,98 @@ import com.prop.persistencia.Database;
 public class Driver1 {
 	// Driver para las clases GeneradorJoc,Jugada,Joc,Partida,Tauler
 	static Database db;
+
+	private static Scanner lector;
+
+	private static Jugador jugador = null;
+	private static Partida partida = null;
+	private static Jugada jugada = null;
+	private static GeneradorJocs generadorJocs = null;
+	private static Joc joc = null;
 	
+	public static void crearGeneradorJocs() {
+		if (generadorJocs != null)
+			System.out.println("Ja existeix un Generador de Jocs");
+		else {
+			System.out.println("Seleccioni si us plau el mode de difícultat del joc: ");
+			System.out.println("  1. Fàcil");
+			System.out.println("  2. Mitjà");
+			System.out.println("  3. Difícil");
+
+			int dif = lector.nextInt();
+
+			if (dif == 1) {
+				System.out.println("Dificultat seleccionada: " + dif);
+				generadorJocs = new GeneradorJocs(20, 5, 3, false, dif);
+			} else if (dif == 2) {
+				System.out.println("Dificultat seleccionada: " + dif);
+				generadorJocs = new GeneradorJocs(25, 6, 3, false, dif);
+			} else if (dif == 3) {
+				System.out.println("Dificultat seleccionada: " + dif);
+				generadorJocs = new GeneradorJocs(25, 7, 3, false, dif);
+			}
+			System.out.println("GeneradorJocs creat amb èxit");
+		}
+	}
 	
-	public static void cas7(Partida partida,Jugador jugador,Jugada jugada) {
+	public static void crearJocDefecte() {
+		if (generadorJocs == null)
+			System.out.println("Primer has de crear un Generador de Jocs");
+		else {
+			if (joc != null)
+				System.out.println("Ja has creat un joc, i en nomès pot haver un");
+			else {
+				joc = generadorJocs.generaJocDefault();
+				System.out.println("Joc creat!");
+			}
+		}
+	}
+	
+	public static void crearJocPersonalitzat() {
+		if (generadorJocs == null)
+			System.out.println("Primer has de crear un Generador de Jocs");
+		else {
+			if (joc != null)
+				System.out.println("Ja has creat un joc, i nomès en pot haver un");
+			else {
+				System.out.println("  Número de files: ");
+				int numFiles = lector.nextInt();
+				System.out.println("  Longitud del codi: ");
+				int longCodi = lector.nextInt();
+				System.out.println("  Número de colors: ");
+				int numColors = lector.nextInt();
+				System.out.println("  Seràs el CodeMaker? (true/false) ");
+				boolean mostraCodi = lector.nextBoolean();
+				System.out.println("  Dificultat (1, 2, 3): ");
+				int dificultat = lector.nextInt();
+				joc = generadorJocs.generaJocCustom(numFiles, longCodi, numColors, mostraCodi, dificultat);
+				System.out.println("Joc creat!");
+			}
+		}
+	}
+	
+	public static void crearPartida() {
+		if (generadorJocs == null)
+			System.out.println("Primer has de crear un Generador de Jocs");
+		else {
+			if (joc == null)
+				System.out.println("Avans has de crear un joc");
+			else {
+				partida = joc.crearPartida();
+				System.out.println("Partida amb id " + partida.getIdPartida() + " creada!");
+			}
+		}
+	}
+	
+	public static void jugarPartida() {
+		
+	}	
+	
+	public static void continuarPartida() {
+		
+	}
+	
+	public static void crearJugada() {
 		if (partida == null)
 			System.out.println("Primer has de crear una partida");
 		else if (jugador == null)
@@ -22,7 +111,7 @@ public class Driver1 {
 		}
 	}
 	
-	public static void cas8(Jugada jugada, Scanner lector) {
+	public static void asignarValorsJugada() {
 		ArrayList<Integer> cp = new ArrayList<Integer>();
 		ArrayList<Integer> cr = new ArrayList<Integer>();
 		if(jugada == null) System.out.println("Primer has de crear una jugada");
@@ -38,6 +127,29 @@ public class Driver1 {
 			jugada.setcodiRespost(cr);
 			System.out.println("S'ha establert correctament el codirespost a la juada creada");
 		}
+	}
+	
+	public static void ferJugada() {
+		
+	}
+	
+	public static void mostraTauler() {
+		if (partida == null)
+			System.out.println("Primer has de crear una partida");
+		else if (jugador == null)
+			System.out.println("Primer has de crear un jugador");
+		else
+			partida.estatTauler();
+	}
+	
+	public static void crearJugador() {
+		Registre reg = new Registre();
+		System.out.println("Identificador del jugador: ");
+		String id = lector.nextLine();
+		ArrayList<Jugador> jugs = new ArrayList<Jugador>();
+		jugs = reg.registrar(id);
+		jugador = jugs.get(0);
+		System.out.println("Jugador creat");
 	}
 	
 	public static void main(String args[]) {
@@ -56,15 +168,9 @@ public class Driver1 {
 
 		db = new Database("MasterMind-Database.txt");
 		db.createDatabase();
-
-		Scanner lector = new Scanner(System.in);
-
-		Jugador jugador = null;
-		Partida partida = null;
-		Jugada jugada = null;
-		GeneradorJocs generadorJocs = null;
-		Joc joc = null;
-
+		
+		lector = new Scanner(System.in);
+		
 		String s;
 		try {
 			Ranking r = new Ranking();
@@ -72,115 +178,46 @@ public class Driver1 {
 
 				switch (s) {
 				case "1": // Crear Generador de Jocs
-					if (generadorJocs != null)
-						System.out.println("Ja existeix un Generador de Jocs");
-					else {
-						System.out.println("Seleccioni si us plau el mode de difícultat del joc: ");
-						System.out.println("  1. Fàcil");
-						System.out.println("  2. Mitjà");
-						System.out.println("  3. Difícil");
-
-						int dif = lector.nextInt();
-
-						if (dif == 1) {
-							System.out.println("Dificultat seleccionada: " + dif);
-							generadorJocs = new GeneradorJocs(20, 5, 3, false, dif);
-						} else if (dif == 2) {
-							System.out.println("Dificultat seleccionada: " + dif);
-							generadorJocs = new GeneradorJocs(25, 6, 3, false, dif);
-						} else if (dif == 3) {
-							System.out.println("Dificultat seleccionada: " + dif);
-							generadorJocs = new GeneradorJocs(25, 7, 3, false, dif);
-						}
-						System.out.println("GeneradorJocs creat amb èxit");
-					}
+					crearGeneradorJocs();
 					break;
 
 				case "2": // Crear Joc per defecte
-					if (generadorJocs == null)
-						System.out.println("Primer has de crear un Generador de Jocs");
-					else {
-						if (joc != null)
-							System.out.println("Ja has creat un joc, i en nomès pot haver un");
-						else {
-							joc = generadorJocs.generaJocDefault();
-							System.out.println("Joc creat!");
-						}
-					}
+					crearJocDefecte();
 					break;
 
 				case "3": // CrearJoc personalitzat
-					if (generadorJocs == null)
-						System.out.println("Primer has de crear un Generador de Jocs");
-					else {
-						if (joc != null)
-							System.out.println("Ja has creat un joc, i nomès en pot haver un");
-						else {
-							System.out.println("  Número de files: ");
-							int numFiles = lector.nextInt();
-							System.out.println("  Longitud del codi: ");
-							int longCodi = lector.nextInt();
-							System.out.println("  Número de colors: ");
-							int numColors = lector.nextInt();
-							System.out.println("  Seràs el CodeMaker? (true/false) ");
-							boolean mostraCodi = lector.nextBoolean();
-							System.out.println("  Dificultat (1, 2, 3): ");
-							int dificultat = lector.nextInt();
-							joc = generadorJocs.generaJocCustom(numFiles, longCodi, numColors, mostraCodi, dificultat);
-							System.out.println("Joc creat!");
-						}
-					}
+					crearJocPersonalitzat();
 					break;
 
 				case "4": // CrearPartida
-					if (generadorJocs == null)
-						System.out.println("Primer has de crear un Generador de Jocs");
-					else {
-						if (joc == null)
-							System.out.println("Avans has de crear un joc");
-						else {
-							partida = joc.crearPartida();
-							System.out.println("Partida amb id " + partida.getIdPartida() + " creada!");
-						}
-					}
+					crearPartida();
 					break;
 
 				case "5": // JugarPartida nova
-
+					jugarPartida();
 					break;
 
 				case "6": // JugarPartida guardada
-
+					continuarPartida();
 					break;
 
 				case "7": // Crear Jugada
-					cas7(partida,jugador,jugada);
+					crearJugada();
 					break;
 				case "8": // Asignar codis a una jugada a una juagada
-					cas8(jugada,lector);
+					asignarValorsJugada();
 					break;
 					
 				case "9": // Fer Jugada
-					
+					ferJugada();
 					break;
 					
-				case "10":
-					if (partida == null)
-						System.out.println("Primer has de crear una partida");
-					else if (jugador == null)
-						System.out.println("Primer has de crear un jugador");
-					else
-						partida.estatTauler();
+				case "10": // Mostrar tauler
+					mostraTauler();
 					break;
 					
 				case "11": // Crear jugador
-					Registre reg = new Registre();
-					System.out.println("Identificador del jugador: ");
-					String id = lector.nextLine();
-					ArrayList<Jugador> jugs = new ArrayList<Jugador>();
-					jugs = reg.registrar(id);
-					jugador = jugs.get(0);
-					System.out.println("Jugador creat");
+					crearJugador();
 					break;
 				}
 			}
