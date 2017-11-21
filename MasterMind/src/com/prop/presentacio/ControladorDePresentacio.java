0cddf12f3f24c3620c9d6006ba711e0a77ea5330
sergilao <sergi.lao97@gmail.com>
2017-11-21 13:07:33 +0100

    controlador presentación ubicado

---
+++ MasterMind/src/com/prop/presentacio/ControladorDePresentacio.java
@@ -0,0 +1,15 @@
+package com.prop.presentacio;
+
+public class ControladorDePresentacio {
+	
+	public void mostra_tauler(String partida) {};
+	
+	public void mostra_menuprincipal() {};
+	
+	public int mostra_partides_disponibles(String[] infopartides) {
+		int opcioseleccionada = 0;
+		return opcioseleccionada;
+	};
+	
+	public void afegeix_codi_respost(String[] codirespost) {};//Mostra el codi repost(vector d'strings) en la vista del tauler
+}
