fc9fee94ac194f3b2ff73ee815a10e6e54c4b0d5
Raúl Valverde Sánchez <raulvalverdesanchez@gmail.com>
2017-11-23 16:26:51 +0100

    Base de Datos de Ranking [Raúl]

---
+++ MasterMind/src/com/prop/persistencia/RankingDatabase.java
@@ -0,0 +1,53 @@
+package com.prop.persistencia;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class RankingDatabase extends Database {
+	private File file;
+	
+	public RankingDatabase(String nom) {
+		super(nom);
+	}
+	
+	public File getRegistreDatabaseFile() {
+		return file;
+	}
+	
+	public void crearDatabase() {
+		file = new File(nom);
+		if (file.exists()) {
+			System.out.println("Ja existeix la base de dades del ranking");
+		}
+		else {
+			try {
+				file = new File(nom);
+				file.createNewFile();
+				FileWriter fw = new FileWriter(file);
+				BufferedWriter bw = new BufferedWriter(fw);
+				bw.write("Ranking de MasterMind");
+				bw.newLine();
+				bw.flush();
+				bw.close();
+				
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				
+				line = br.readLine();
+				System.out.println(line);
+				br.close();
+				
+				System.out.println("Base de dades creada correctament");
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+
+}
