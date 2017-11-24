c1ff6cd0ca75deaca00ff9eccde057421c49e868
sergilao <sergi.lao97@gmail.com>
2017-11-24 17:52:17 +0100

    Controlador Persistencia

--- MasterMind/src/com/prop/persistencia/ControladorDePersistencia.java
+++ MasterMind/src/com/prop/persistencia/ControladorDePersistencia.java
@@ -1,40 +1,17 @@
 package com.prop.persistencia;
 
-
+import com.prop.domini.Jugador;
 public class ControladorDePersistencia {
-	
-		RegistreDatabase regDB = new RegistreDatabase("RegistreJugadors.txt");
-		PartidesDatabase partDB = new PartidesDatabase("Partides.txt");
-		RankingDatabase rankDB = new RankingDatabase("RankingJugadors.txt");
 
-		/* Guarda info a una base de datos que puede ser la del ranking, registro o partida (puede que haya alguna mas) */
-		/* Parámetros: 
-		 * int modo: 0 si es para almacenar en la bd de registro
-		 * 			 1 si es para almacenar en la bd de partida
-		 * 			 2 si es para almacenar en la bd de ranking 
-		 * String[] info: vector con la info del objeto segun el modo que se haya pasado */
-		public void emmagatzema(int mode, String[] info) {
-			if (mode == 0) { //registro jugadores
-				regDB.emmagatzemaJugador(info);
-			}
-			else if (mode == 1) { //partidas
-				
-			}
-			else if (mode == 2) { //ranking
-				
-			}
-		}
+		public void emmagatzema_partida(String[] partida,String idjugador) {};
 		
-		/* Consulta jugador */
-		public String[] consultaJugador(String id) {
-			String[] jugador = null;
-			return jugador;
-		}
+		public void emmagatzema_ranking(String[][] ranking) {};
 		
-		//public void partides_no_finalitzades(String nomjugador) {}
+		public void emmagatzema_jugador(String[] idjugador) {};
 		
-		//public String[] obteranking() {}
+		public void partides_no_finalitzades(String nomjugador) {};
 		
-		//public String[] obtepartidesjugador(String nomjugador) {}
+		public String[][] obteranking() {};
 		
+		public String[][] obtepartidesjugador(String nomjugador) {};
 }
