ce71c5eeeac7981abdf7dfeae39f0b5671907898
Raúl Valverde Sánchez <raulvalverdesanchez@gmail.com>
2017-12-06 22:26:13 +0100

    Actualización controlador persistencia [Raúl]

--- MasterMind/src/com/prop/persistencia/ControladorDePersistencia.java
+++ MasterMind/src/com/prop/persistencia/ControladorDePersistencia.java
@@ -1,17 +1,47 @@
 package com.prop.persistencia;
 
-import com.prop.domini.Jugador;
 public class ControladorDePersistencia {
-
-		public void emmagatzema_partida(String[] partida,String idjugador) {};
+	RegistreDatabase regDB;
+	PartidesDatabase partDB;
+	RankingDatabase rankDB;
+	
+	public ControladorDePersistencia() {
+		regDB = new RegistreDatabase("RegistreJugadors.txt");
+		partDB = new PartidesDatabase("Partides.txt");
+		rankDB = new RankingDatabase("RankingJugadors.txt");
+	}
+	
+	//Se ejecuta la primera vez que se ejecuta el programa, luego en las demas ejecuciones solo hace print como que ya se han creado las databases
+	public void inicialitzaDatabases() {
+		regDB.crearDatabase();
+		partDB.crearDatabase();
+		rankDB.crearDatabase();
+	}
+	
+	public void emmagatzemaJugador(String[] info) {
+		regDB.emmagatzemaJugador(info);
+	}
+	
+	public void emmagatzemaPartida(String[] partida,String idjugador) {
 		
-		public void emmagatzema_ranking(String[][] ranking) {};
+	}
 		
-		public void emmagatzema_jugador(String[] idjugador) {};
+	public void emmagatzemaRanking(String[][] ranking) {
+			
+	}
 		
-		public void partides_no_finalitzades(String nomjugador) {};
+	public void partidesNoFinalitzades(String nomjugador) {
 		
-		public String[][] obteranking() {};
+	}
 		
-		public String[][] obtepartidesjugador(String nomjugador) {};
+	public String[][] obteRanking() {
+		String[][] asdf = null;
+		return asdf;
+	}
+		
+	public String[][] obtePartidesJugador(String nomjugador) {
+		String[][] asdf = null;
+		return asdf;
+	}
+	
 }
