d2d3ddcda5490bf78b2ce98717f6ee74fbf6fed4
sergilao <sergi.lao97@gmail.com>
2017-12-15 16:51:19 +0100

    Vista Partida Guardada OK

---
+++ MasterMind/src/com/prop/persistencia/ControladorDePresentacio.java
@@ -0,0 +1,58 @@
+package com.prop.presentacio;
+import com.prop.domini.ControladorDeDomini;
+
+public class ControladorDePresentacio {
+	
+	ControladorDeDomini c;
+	String nom;
+	
+	public ControladorDePresentacio() {
+		this.c = new ControladorDeDomini();
+		nom = null;
+	}
+	
+	public void mostra_tauler(String[] partida) {};
+	
+	public void mostra_menuprincipal() {};
+	
+	public int obteseleccionada() {
+		int d = 4;
+		return d;
+	};
+	
+	public String[] mostra_partides_disponibles(String[][] infopartides) {
+		int opcioseleccionada = obteseleccionada();
+		return infopartides[opcioseleccionada];
+	};
+	
+	public void afegeix_codi_respost(String[] codirespost) {};//Mostra el codi repost(vector d'strings) en la vista del tauler
+	
+	public boolean click_registrarse(String alies) { //retorna true si s'ha creat el jugador, retorna false si ja exiteix
+		nom = alies;
+		boolean b = false;
+		return b;
+	};
+	
+	public void crear_partida(String mode, String dificultat) {
+		switch(dificultat) { //Crida al controlador de domini indicant el mode de partida i la dificultat de la partida
+			case "Facil":
+				
+			break;
+			case "Mitjana":
+				
+			break;
+			case "Dificil":
+		}
+	}
+	public String[] demana_ranking() { //Demana el ranking a la capa de domini i dona el format necessari per mostrar-ho per pantalla
+		String[] ranking = new String[2];
+		ranking[0]="1.  Ro  23";
+		ranking[1]="2.  Pep 40";
+		
+		//ranking = c.consultar_ranking();
+		return ranking;
+	}
+	public String[] getIdPartidesGuardades() {//Demana al controlador de domini els id's de les partides guardades.
+		return c.getIdPartidesGuardades(nom);
+	}
+}
