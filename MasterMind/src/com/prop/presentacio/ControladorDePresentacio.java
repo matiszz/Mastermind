bc2d2d51df6118aabeb81ce50bc9c114363749d5
sergilao <sergi.lao97@gmail.com>
2018-01-04 19:40:18 +0100

    presentacio update

--- MasterMind/src/com/prop/presentacio/ControladorDePresentacio.java
+++ MasterMind/src/com/prop/presentacio/ControladorDePresentacio.java
@@ -24,6 +24,7 @@ public class ControladorDePresentacio {
         // Constructor
 	public ControladorDePresentacio() {
             c = new ControladorDeDomini(this);
+            c.instanciaRanking();
             nomJugador = null;
 	}
         
